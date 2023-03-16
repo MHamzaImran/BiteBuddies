5b0abeebbe266feacbda4bffd7f8d6cf1eb5d4fd
MHamzaImran <hamzaimran.124.8@gmail.com>
2023-03-17 00:28:28 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:bite_buddies/screens/more/changeAddressScreen.dart';
+
+import 'screens/authentication/spashScreen.dart';
+import 'screens/authentication/landingScreen.dart';
+import 'screens/authentication/loginScreen.dart';
+import 'screens/authentication/signUpScreen.dart';
+import 'screens/authentication/forgetPwScreen.dart';
+import 'screens/authentication/sentOTPScreen.dart';
+import 'screens/authentication/newPwScreen.dart';
+import 'screens/intro/introScreen.dart';
+import 'screens/home/homeScreen.dart';
+import 'screens/menu/menuScreen.dart';
+import 'screens/more/moreScreen.dart';
+import 'screens/offers/offerScreen.dart';
+import 'screens/profile/profileScreen.dart';
+import 'screens/menu/dessertScreen.dart';
+import 'screens/home/individualItem.dart';
+import 'screens/more/paymentScreen.dart';
+import 'screens/more/notificationScreen.dart';
+import 'screens/more/aboutScreen.dart';
+import 'screens/more/inboxScreen.dart';
+import 'screens/more/myOrderScreen.dart';
+import 'screens/more/checkoutScreen.dart';
+import './const/colors.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: "Metropolis",
+        primarySwatch: Colors.red,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+              AppColor.main,
+            ),
+            shape: MaterialStateProperty.all(
+              StadiumBorder(),
+            ),
+            elevation: MaterialStateProperty.all(0),
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all(
+              AppColor.main,
+            ),
+          ),
+        ),
+        textTheme: TextTheme(
+          headline3: TextStyle(
+            color: AppColor.primary,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: TextStyle(
+            color: AppColor.secondary,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+          headline5: TextStyle(
+            color: AppColor.primary,
+            fontWeight: FontWeight.normal,
+            fontSize: 25,
+          ),
+          headline6: TextStyle(
+            color: AppColor.primary,
+            fontSize: 25,
+          ),
+          bodyText2: TextStyle(
+            color: AppColor.secondary,
+          ),
+        ),
+      ),
+      home: SplashScreen(),
+      routes: {
+        LandingScreen.routeName: (context) => LandingScreen(),
+        LoginScreen.routeName: (context) => LoginScreen(),
+        SignUpScreen.routeName: (context) => SignUpScreen(),
+        ForgetPwScreen.routeName: (context) => ForgetPwScreen(),
+        SendOTPScreen.routeName: (context) => SendOTPScreen(),
+        NewPwScreen.routeName: (context) => NewPwScreen(),
+        IntroScreen.routeName: (context) => IntroScreen(),
+        HomeScreen.routeName: (context) => HomeScreen(),
+        MenuScreen.routeName: (context) => MenuScreen(),
+        OfferScreen.routeName: (context) => OfferScreen(),
+        ProfileScreen.routeName: (context) => ProfileScreen(),
+        MoreScreen.routeName: (context) => MoreScreen(),
+        DessertScreen.routeName: (context) => DessertScreen(),
+        IndividualItem.routeName: (context) => IndividualItem(),
+        PaymentScreen.routeName: (context) => PaymentScreen(),
+        NotificationScreen.routeName: (context) => NotificationScreen(),
+        AboutScreen.routeName: (context) => AboutScreen(),
+        InboxScreen.routeName: (context) => InboxScreen(),
+        MyOrderScreen.routeName: (context) => MyOrderScreen(),
+        CheckoutScreen.routeName: (context) => CheckoutScreen(),
+        ChangeAddressScreen.routeName: (context) => ChangeAddressScreen(),
+      },
+    );
+  }
+}
